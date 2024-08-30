a65379c8fc47834da851a473d3980c36555ab8af
Syeda Umme Hani <112943478+syedasuhum@users.noreply.github.com>
2024-08-30 16:35:42 +0530

    Add files via upload

---
+++ Weather_app/Default.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Async="true" CodeFile="Default.aspx.cs" Inherits="Weather_app_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Weather App</title>
+    <style>
+        body{
+            margin:20px;
+            font-weight:500;
+            color:purple;
+            /* background-color: red;*/ /* For browsers that do not support gradients */
+  /*color: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);*/
+        }
+       
+       .mybody {
+/*background-position:center;
+  filter:blur(2px);
+   background-image:url(img.jpg);
+            background-repeat:no-repeat;
+             height:600px;*/
+position: absolute; 
+            height: 600px; 
+            width: 600px; 
+            background-image:url(img.jpg); 
+            background-size: cover; 
+            background-position: center; 
+            filter: blur(2px); 
+            z-index: -1;
+            justify-items:center;
+            margin-left:350px;
+}
+        .container{
+          /*  border:1px solid gray;*/
+            padding:25px;
+           
+            
+          /*  background-color:#ccc;*/
+            /*background-color: red;*/ /* For browsers that do not support gradients */
+            /*background-image: linear-gradient(red, yellow);*/
+  width:600px;
+   text-align:center;
+/*   position:absolute;*/
+   
+   
+   margin-left:350px;
+    /* box-shadow: 10px 10px 5px lightblue;*/
+        }
+
+        .text{
+            font-size:18px;
+            font-weight:600;
+            
+        }
+        .input_text{
+            border:1px solid black;
+            border-radius:5px;
+            height:25px;
+             background-color: transparent;
+           
+        }
+        .btn{
+            color:white;
+            background-color:black;
+            width:auto;
+            height:25px;
+            border:none;
+            font-size:15px;
+            border-radius:5px;
+            font-weight:600;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="mybody"></div>
+           
+            <div class="container">
+                 <h1>Weather App</h1>
+            <label for="txtCityName" class="text">Enter City/State Name:</label>
+            <asp:TextBox ID="txtCityName"  CssClass="input_text" runat="server"></asp:TextBox>
+            <asp:Button ID="btnGetWeather" runat="server" CssClass="btn" Text="Get Weather" OnClick="btnGetWeather_Click" />
+            
+            <asp:Label ID="lblWeatherInfo" runat="server" Visible="false"></asp:Label>
+        </div>
+            
+    </form>
+</body>
+</html>
---
+++ Weather_app/Default.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Net.Http;
+using Newtonsoft.Json;
+
+public partial class Weather_app_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected async void btnGetWeather_Click(object sender, EventArgs e)
+    {
+        string cityName = txtCityName.Text.Trim();
+
+        // Replace spaces in city name with %20 for URL
+        string cityQuery = cityName.Replace(" ", "%20");
+
+        // OpenWeatherMap API endpoint for current weather
+        string apiKey = "09e82a3cdc8c8c925fef0c8f8abfa7d3"; // Replace with your own API key
+        string apiUrl = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units=metric", cityQuery, apiKey);
+
+
+        // Create HttpClient
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                // Send GET request to API
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                // Check if request was successful
+                if (response.IsSuccessStatusCode)
+                {
+                    // Read response content
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    // Deserialize JSON response
+                    WeatherData weatherData = JsonConvert.DeserializeObject<WeatherData>(json);
+
+                    // Display weather information
+                   
+                    string weatherInfo = "<h1>Weather in " + weatherData.Name + ", " + weatherData.Sys.Country + ":</h3>"
++ "<h2>Description: " + weatherData.Weather[0].Description + "</p>"
++ "<h2>Temperature: " + weatherData.Main.Temp + "°C</p>"
++ "<h2>Feels like: " + weatherData.Main.FeelsLike + "°C</p>"
++"<h2> Humidity:" +weatherData.Main.Humidity + "°C</p>"
+                                + "<p>Wind speed:" + weatherData.Wind.Speed +" m/s</p>";
+
+
+                    lblWeatherInfo.Text = weatherInfo;
+                    lblWeatherInfo.Visible = true;
+                }
+                else
+                {
+                    lblWeatherInfo.Text = "Failed to retrieve weather data. Status code: " + response.StatusCode;
+
+                    lblWeatherInfo.Visible = true;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                lblWeatherInfo.Text = "Error: " + ex.Message;
+
+                lblWeatherInfo.Visible = true;
+            }
+        }
+    }
+
+
+// Define classes to represent JSON response
+public class WeatherData
+{
+    public Weather[] Weather { get; set; }
+    public MainData Main { get; set; }
+    public WindData Wind { get; set; }
+    public string Name { get; set; }
+    public SysData Sys { get; set; }
+}
+
+public class Weather
+{
+    public string Description { get; set; }
+}
+
+public class MainData
+{
+    public float Temp { get; set; }
+    public float FeelsLike { get; set; }
+    public int Humidity { get; set; }
+}
+
+public class WindData
+{
+    public float Speed { get; set; }
+}
+
+public class SysData
+{
+    public string Country { get; set; }
+}
+
+}
